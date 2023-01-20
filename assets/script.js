4c1219898bea4800cfb3473fd7c9f344de322794
cbarnes0 <carsonbarnes98@gmail.com>
2023-01-20 13:47:35 -0500

    minor changes to css and html. main thing: buttons appear when start is pushed using JS

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,20 @@
 var timerElement = document.querySelector(".timer-count");
 var startButtonEl = document.querySelector(".startButton");
+var answerButtomsEl = document.getElementsByClassName("answerButtons");
 
 var timer;
 var timerCount;
+
 
 // This is JUST TO TEST timer CURRENTLY !!!
 startButtonEl.addEventListener("click", function() {
     timerElement.textContent = 5;
     timerCount = 5;
     startTimer();
-})
-
+    buttonsAppear();
+    
+});
+// This is the function that starts the timer
 function startTimer() {
     // Sets timer
     timer = setInterval(function() {
@@ -20,4 +24,18 @@ function startTimer() {
             clearInterval(timer);
         }
     }, 1000);
-  }
+  };
+  
+// This is the function that makes the 'answer' buttons appear
+function buttonsAppear() {
+    let answers = [
+        document.getElementById('answer1'),
+        document.getElementById('answer2'),
+        document.getElementById('answer3'),
+        document.getElementById('answer4')];
+
+    for (let i=0; i < answers.length; i++) {
+        
+        answers[i].setAttribute('style', 'display: block;' );
+    };
+}
